f7e9ced300c3c96316c07cede84d375168c8a504
m_brue <m.brueggemann7@web.de>
2024-04-03 16:21:16 +0200

    add extra keybind

--- lua/config/vim_slime.lua
+++ lua/config/vim_slime.lua
@@ -1,3 +1,6 @@
 vim.g.slime_target = "neovim" -- all "vim-slime" commands will be send to neovims integrated terminal
 
 -- to send something to the "slime_target" press: ctrl-c-c (you can hold ctrl)
+vim.keymap.set('v', '<leader>s', ':SlimeRegionSend', {})
+
+
