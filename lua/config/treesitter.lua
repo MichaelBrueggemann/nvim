c871ceb2ab4678ad3e2a60e6e63865158a3255a1
m_brue <m.brueggemann7@web.de>
2025-07-02 01:51:06 +0200

    remove unused treesitter packages

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,7 +1,5 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "julia", "java", "typst" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typst" },
     auto_install = true,
     highlight = { enable = true},
 })
-
-
