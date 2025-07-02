dfb0a9af0fc3419cdfa0f76272b9d2bdbdd7eab5
m_brue <m.brueggemann7@web.de>
2025-07-02 02:00:53 +0200

    add comment.nvim package

--- lua/settings/plugins.lua
+++ lua/settings/plugins.lua
@@ -30,11 +30,19 @@ require("lazy").setup({
     { "hrsh7th/cmp-path" },
     { "hrsh7th/cmp-cmdline" },
 
-    -- open/close brackets
+    -- OPEN/CLOSE BRACKETS
     {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
         config = true, -- Enables default setup
+    },
+
+    -- COMMENTS
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        }
     },
 
     -- ATOM ONE DARK COLORSCHEME
