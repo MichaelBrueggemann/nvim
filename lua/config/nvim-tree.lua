5be521a1d437330ae2c2c00fe786e133a5a6797d
m_brue <m.brueggemann7@web.de>
2025-07-02 11:00:41 +0200

    add keymaps for nvim-tree

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -5,8 +5,20 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
-require("nvim-tree").setup()
-
+local nvim_tree = require("nvim-tree")
+local nvim_tree_view = require('nvim-tree.view')
+nvim_tree.setup()
 
 -- KEYMAPS
-vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>')
+-- open/close the file tree
+-- find the currently opened file in the file tree
+vim.keymap.set('n', '<leader>t', function()
+  if require('nvim-tree.view').is_visible() then
+    vim.cmd('NvimTreeToggle')
+  else
+    vim.cmd('NvimTreeFocus')
+  end
+end, { desc = 'Toggle or focus NvimTree' })
+vim.keymap.set('n', '<leader>tff', ':NvimTreeFindFile<CR>')
+-- collapse the file tree recursively
+vim.keymap.set('n', '<leader>tc', ':NvimTreeCollapse<CR>')
