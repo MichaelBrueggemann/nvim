b814fd066f7e0d07ad76eb4736c9761ed5a352b4
m_brue <m.brueggemann7@web.de>
2025-07-04 15:55:30 +0200

    enable line folding

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -3,3 +3,7 @@ require("nvim-treesitter.configs").setup({
     auto_install = true,
     highlight = { enable = true},
 })
+
+-- enable treesitter based expression folding
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -20,7 +20,7 @@ vim.opt.relativenumber = false -- add numbers to each line on the left side
 vim.opt.cursorline = false     -- highlight cursor line underneath the cursor horizontally
 vim.opt.splitbelow = true      -- open new vertical split bottom
 vim.opt.splitright = true      -- open new horizontal splits right
--- vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
+vim.opt.termguicolors = true        -- enable 24-bit RGB color in the TUI
 vim.opt.showmode = true        -- we are experienced, wo don't need the "-- INSERT --" mode hint
 
 -- Searching
@@ -28,3 +28,6 @@ vim.opt.incsearch = true  -- search as characters are entered
 vim.opt.hlsearch = false  -- do not highlight matches
 vim.opt.ignorecase = true -- ignore case in searches by default
 vim.opt.smartcase = true  -- but make it case sensitive if an uppercase is entered
+
+-- Line Folding
+vim.wo.foldminlines = 10 -- only fold blocks that are longer than the stated number
